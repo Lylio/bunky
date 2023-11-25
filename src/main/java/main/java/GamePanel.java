c3d3eaa0452cb1469eae9a91dd2c30777df85f5d
Lylio <cxz@gmx.com>
2023-11-25 06:02:22 +0000

    first commit

---
+++ src/main/java/main/java/GamePanel.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tilesize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = maxScreenCol * tilesize; // 768 pixels
+    final int screenHeight = maxScreenRow * tilesize; // 576 pixels
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+}
