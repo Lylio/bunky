dcea7e3579681d356eb8fa23f0e95d2b84b09ce7
Lylio <cxz@gmx.com>
2024-01-08 14:25:58 +0000

    chore : Key Handler Boolean fix

--- src/main/java/com/lylechristine/bunky/GamePanel.java
+++ src/main/java/com/lylechristine/bunky/GamePanel.java
@@ -9,19 +9,27 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tilesize = originalTileSize * scale; // 48x48 tile
+    final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = maxScreenCol * tilesize; // 768 pixels
-    final int screenHeight = maxScreenRow * tilesize; // 576 pixels
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -33,5 +41,43 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        while (gameThread != null) {
+
+            long currentTime = System.nanoTime();
+            System.out.println("current time: " + currentTime);
+
+            // 1. UPDATE : update information such as character positions
+            update();
+
+            // 2.DRAW: draw the screen with the updated information
+            repaint();
+        }
+
+    }
+
+    public void update() {
+
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        } else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
+    }
+    public void paintComponent (Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
