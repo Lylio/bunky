2dbb2bcc8b0187108f8930d666a5ca607b049ebb
Lylio <cxz@gmx.com>
2024-01-17 23:15:36 +0000

    feat : world map and camera : video #5

--- src/main/java/com/lylechristine/bunky/tile/TileManager.java
+++ src/main/java/com/lylechristine/bunky/tile/TileManager.java
@@ -20,9 +20,9 @@ public class TileManager {
         this.gp = gp;
 
         tile = new Tile[10];
-        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+        mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
         getTileImage();
-        loadMap();
+        loadMap("/images/maps/world01.txt");
     }
 
     public void getTileImage() {
@@ -38,26 +38,46 @@ public class TileManager {
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/images/tiles/water.png"));
 
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/images/tiles/earth.png"));
+
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/images/tiles/tree.png"));
+
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/images/tiles/sand.png"));
+
         } catch(IOException e) {
             e.printStackTrace();
         }
 
     }
-    public void loadMap() {
+    public void loadMap(String filePath) {
         try {
-            InputStream is = getClass().getResourceAsStream("/images/maps/map01.txt");
+            InputStream is = getClass().getResourceAsStream(filePath);
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            String line;
+            int col = 0;
             int row = 0;
 
-            while (row < gp.maxScreenRow && (line = br.readLine()) != null) {
-                String numbers[] = line.split(" ");
-                for (int col = 0; col < gp.maxScreenCol && col < numbers.length; col++) {
+            while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+
+                String line = br.readLine();
+
+                while (col < gp.maxWorldCol) {
+
+                    String numbers[] = line.split(" ");
+
                     int num = Integer.parseInt(numbers[col]);
-                    mapTileNum[col][row] = num;
+
+                    mapTileNum[row][col] = num;
+                    col++;
                 }
-                row++;
+
+                if (col == gp.maxWorldCol) {
+                    col = 0;
+                    row++;
+                }
             }
             br.close();
 
@@ -66,24 +86,31 @@ public class TileManager {
         }
     }
     public void draw(Graphics2D g2) {
-        int col = 0;
-        int row = 0;
-        int x = 0;
-        int y = 0;
+        int worldCol = 0;
+        int worldRow = 0;
 
-        while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+        while(worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
 
-            int tileNum = mapTileNum[col][row];
+            int tileNum = mapTileNum[worldCol][worldRow];
 
-            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
-            col++;
-            x += gp.tileSize;
+            int worldX = worldCol * gp.tileSize;
+            int worldY = worldRow * gp.tileSize;
+            int screenX = worldX - gp.player.worldX + gp.player.screenX;
+            int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-            if(col == gp.maxScreenCol) {
-                col = 0;
-                x = 0;
-                row++;
-                y += gp.tileSize;
+            if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+               worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+               worldY + gp.tileSize > gp.player.worldX - gp.player.screenY &&
+               worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+
+                g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+            }
+
+            worldCol++;
+
+            if(worldCol == gp.maxWorldCol) {
+                worldCol = 0;
+                worldRow++;
             }
         }
     }
